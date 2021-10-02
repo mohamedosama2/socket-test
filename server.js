00250aed27485f5353d90ec18fb6f4a01c5630f0
mohamedosama2 <latokalatohamato@gmail.com>
2021-10-02 22:55:43 +0200

    make it better

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express"),
+  cors = require("cors"),
+  socketio = require("socket.io"),
+  { createServer } = require("http"),
+  router = require("./routers/router");
+
+const app = express(),
+  server = createServer(app),
+  PORT = process.env.PORT || 8000,
+  io = socketio(server);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(cors());
+app.use(router);
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+  socket.on("disconnect", () => console.log("user discoonected"));
+});
+server.listen(PORT, () => console.log(`server is running in port ${PORT}`));
